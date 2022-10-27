0eda156833602ee47e2fb20e0985ab9ce10851da
Ikaros-Black <andresbenitez_scarlet@hotmail.com>
2022-10-27 16:02:39 -0300

    correccion

--- src/main/java/com/Portfolio/Benitez/Controller/CEducacion.java
+++ src/main/java/com/Portfolio/Benitez/Controller/CEducacion.java
@@ -24,19 +24,19 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-@RequestMapping("/educacion")
-@CrossOrigin(origins = "https://ikarosfrontend.web.app")
+@RequestMapping("educacion/")
+@CrossOrigin(origins = "https://ikarosfrontend.web.app/")
 public class CEducacion {
     @Autowired
     SEducacion sEducacion;
     
-    @GetMapping("/lista")
+    @GetMapping("lista")
     public ResponseEntity<List<Educacion>> list(){
         List<Educacion> list = sEducacion.list();
         return new ResponseEntity(list, HttpStatus.OK);
     }
     
-    @GetMapping("/detail/{id}")
+    @GetMapping("detail/{id}")
     public ResponseEntity<Educacion> getById(@PathVariable("id")int id){
         if(!sEducacion.existsById(id)){
             return new ResponseEntity(new Mensaje("No existe el ID"), HttpStatus.BAD_REQUEST);
@@ -46,7 +46,7 @@ public class CEducacion {
         return new ResponseEntity(educacion, HttpStatus.OK);
     }
     
-    @DeleteMapping("/delete/{id}")
+    @DeleteMapping("delete/{id}")
     public ResponseEntity<?> delete(@PathVariable("id") int id){
         if(!sEducacion.existsById(id)){
             return new ResponseEntity(new Mensaje("No existe el ID"), HttpStatus.NOT_FOUND);
@@ -55,7 +55,7 @@ public class CEducacion {
         return new ResponseEntity(new Mensaje("Educacion eliminada"), HttpStatus.OK);
     }
     
-    @PostMapping("/create")
+    @PostMapping("create")
     public ResponseEntity<?> crete(@RequestBody dtoEducacion dtoeducacion){
         if(StringUtils.isBlank(dtoeducacion.getNombreE())){
             return new ResponseEntity(new Mensaje("El nombre es oblligatoprio"), HttpStatus.BAD_REQUEST);
@@ -71,7 +71,7 @@ public class CEducacion {
         return new ResponseEntity(new Mensaje("Educacion Creada"), HttpStatus.OK);
     }
     
-    @PutMapping("/update/{id}")
+    @PutMapping("update/{id}")
     public ResponseEntity<?> update(@PathVariable("id") int id, @RequestBody dtoEducacion dtoeducacion){
         if(!sEducacion.existsById(id)){
             return new ResponseEntity(new Mensaje("No existe el ID"), HttpStatus.NOT_FOUND);
--- src/main/java/com/Portfolio/Benitez/Security/Controller/AuthController.java
+++ src/main/java/com/Portfolio/Benitez/Security/Controller/AuthController.java
@@ -33,8 +33,8 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-@RequestMapping("/auth")
-@CrossOrigin(origins = "https://ikarosfrontend.web.app")
+@RequestMapping("auth/")
+@CrossOrigin(origins = "https://ikarosfrontend.web.app/")
 public class AuthController {
     @Autowired
     PasswordEncoder passwordEncoder;
@@ -47,7 +47,7 @@ public class AuthController {
     @Autowired
     JwtProvider jwtProvider;
     
-    @PostMapping("/nuevo")
+    @PostMapping("nuevo")
     public ResponseEntity<?> nuevo(@Valid @RequestBody NuevoUsuario nuevoUsuario, BindingResult bindingResult){
         if(bindingResult.hasErrors())
             return new ResponseEntity(new Mensaje("Campo erroneo o email invalido"),HttpStatus.BAD_REQUEST );
@@ -73,7 +73,7 @@ public class AuthController {
         return new ResponseEntity(new Mensaje("Usuario guardado"),HttpStatus.CREATED);
     }
     
-    @PostMapping("/login")
+    @PostMapping("login")
     public ResponseEntity<JwtDto> login(@Valid @RequestBody LoginUsuario loginUsuario, BindingResult bindingResult){
         if(bindingResult.hasErrors())
             return new ResponseEntity(new Mensaje("Campos incorrectos"), HttpStatus.BAD_REQUEST);
